388e4f9dc967396612cf086c59de7e44a1eaaa7c
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-21 14:10:47 +0530

    commits

--- src/main/java/com/electra/model/Address.java
+++ src/main/java/com/electra/model/Address.java
@@ -2,19 +2,69 @@ package com.electra.model;
 
 public class Address {
     private Long id;
-
     private String street;
-
     private String city;
-
     private String state;
-
-
     private String country;
+    private long postalCode;
 
-
-    private String postalCode;
-
-        // Getters and Setters
+    public Address(Long id, String street, String city, String state, String country, long postalCode) {
+        this.id = id;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.postalCode = postalCode;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public long getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(long postalCode) {
+        this.postalCode = postalCode;
+    }
+
+
+}
 
--- src/main/java/com/electra/repository/AddressRepository.java
+++ src/main/java/com/electra/repository/AddressRepository.java
@@ -19,23 +19,26 @@ public class AddressRepository {
             connection = new ConnectionService().getConnection();
         }
     }
-
     public List<Address> retrieveAddresses() {
-
         List<Address> addresses = new ArrayList<>();
-
         // Use the connection to execute SQL queries and interact with the database
         try {
             this.initConnection();
+
             // Your database operations here...
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM address");
+
             // Iterate over the result set
             while (resultSet.next()) {
-                int id = resultSet.getInt("address_id"); // Replace "id" with your actual column name
+                long id = resultSet.getLong("id"); // Replace "id" with your actual column name
                 String city = resultSet.getString("city"); // Replace "city" with your actual column name
+                String street = resultSet.getString("street");
+                String state = resultSet.getString("state");
+                String country = resultSet.getString("country");
+                long postal_code = resultSet.getLong("postal_code");
                 // Do something with the data, e.g., print it
-                Address address = new Address();
+                Address address = new Address(id, street, city,  state,  country, postal_code);
                 addresses.add(address);
             }
         } catch (SQLException e) {
@@ -52,35 +55,4 @@ public class AddressRepository {
         }
         return addresses;
     }
-
-    public Address retrieveAddress(int addressId) {
-        Address address = null;
-        // Use the connection to execute SQL queries and interact with the database
-        try {
-            this.initConnection();
-            // Your database operations here...
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM address where address_id = " + addressId);
-            // Iterate over the result set
-            while (resultSet.next()) {
-                int id = resultSet.getInt("address_id"); // Replace "id" with your actual column name
-                String city = resultSet.getString("city"); // Replace "city" with your actual column name
-                // Do something with the data, e.g., print it
-                address = new Address();
-            }
-        } catch (SQLException e) {
-            System.err.println("SQL error: " + e.getMessage());
-        } finally {
-            // Close the connection when done
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    System.err.println("Error closing connection: " + e.getMessage());
-                }
-            }
-        }
-        return address;
-    }
-
 }
