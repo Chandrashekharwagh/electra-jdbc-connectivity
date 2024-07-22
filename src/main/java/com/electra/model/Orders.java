573c3020d93695bda3e70a2eab87941158146c5c
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-22 20:15:25 +0530

    commits

---
+++ src/main/java/com/electra/model/Orders.java
@@ -0,0 +1,49 @@
+package com.electra.model;
+
+import java.util.Date;
+
+public class Orders {
+    private Long id;
+    private Product product;
+    private Customer customer;
+    private Date orderDate;
+
+    public Orders(Long id, Product product, Customer customer, Date orderDate) {
+        this.id = id;
+        this.product = product;
+        this.customer = customer;
+        this.orderDate = orderDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+}
