1c1716def123ca01c53627f3c2264f057b868f47
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-22 15:15:12 +0530

    commits

--- src/main/java/com/electra/model/Brand.java
+++ src/main/java/com/electra/model/Brand.java
@@ -2,12 +2,48 @@ package com.electra.model;
 
 public class Brand {
     private Long id;
-
     private String name;
-
     private String description;
+    private String address;
 
-    private Address address;
+    public Brand(Long id, String name, String description, String address) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.address = address;
+    }
 
-    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = String.valueOf(address);
+    }
+
+
 }
