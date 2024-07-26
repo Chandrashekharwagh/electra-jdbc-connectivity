62a5b7e19a73fa666bf195d19a6f4ede29e01094
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-26 09:41:08 +0530

    commits

--- src/main/java/com/electra/service/SupplierService.java
+++ src/main/java/com/electra/service/SupplierService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class SupplierService {
     private static final SupplierRepository SUPPLIER_REPO = new SupplierRepository();
 
+    // Method to retrieve supplier of the database
     public List<Supplier> retrieveSupplier() {
         return SUPPLIER_REPO.retrieveSupplier();
 
