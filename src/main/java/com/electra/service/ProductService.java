600eb9f8c471508c34a5c7e4753013dc7d423d42
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-07-26 09:40:50 +0530

    commits

--- src/main/java/com/electra/service/ProductService.java
+++ src/main/java/com/electra/service/ProductService.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class ProductService {
     private static final ProductRepository PRODUCT_REPO = new ProductRepository();
 
+    // Method to retrieve products of the database
     public static List<Product> retrieveProducts() {
         return PRODUCT_REPO.retrieveProducts();
 
